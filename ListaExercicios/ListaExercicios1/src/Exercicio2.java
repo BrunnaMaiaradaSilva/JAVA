452b518ff3982f5ea42121142176d1c707ae7fb3
Brunna Silva <silva.brunnam@gmail.com>
2020-08-17 16:51:27 -0300

    :zap:

--- ListaExercicios/ListaExercicios1/src/Exercicio2.java
+++ ListaExercicios/ListaExercicios1/src/Exercicio2.java
@@ -1,4 +1,15 @@
 
 public class Exercicio2 {
-
+	
+	public double nota1;
+	public double nota2;
+	public double nota3;
+	
+	public double calculaMedia(double nota1, double nota2, double nota3) {
+		nota1 = this.nota1;
+		nota2 = this.nota2;
+		nota3 = this.nota3;
+		
+		return nota1 + nota2 + nota3 / 3;
+	}
 }
